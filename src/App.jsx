0496d70086c2f2168ef88e8ff2e7462763039408
Andrew Shen <azshen23@gmail.com>
2023-08-08 11:47:44 -0400

    mobile kinda optimized

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,9 @@ function App() {
       <AnimatePresence>
         {isZoomed && (
           <motion.div
-            className="box absolute bottom-10 left-0 right-0 text-xl m-auto rounded-md pt-8 pr-8 pl-8 w-3/5 h-56 bg-white leading-10 border-8 border-white"
+            className="box absolute bottom-10 left-0 right-0 text-sm md:text-base lg:text-base xl:text-lg 2xl:text-xl md:m-auto
+            rounded-md pt-6 lg:pt-8 pr-8 pl-2 sm:pl-0 md:pl-8 sm:w-full md:w-3/5 sm:h-60 md:h-56 bg-white 
+            xl:leading-8 2xl:leading-10 border-8 border-white"
             initial={{ x: 300, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             exit={{ x: -300, opacity: 0 }}
@@ -126,8 +128,9 @@ function App() {
         )}
         {dialogueFin && isZoomed && (
           <motion.div
-            className="box absolute bottom-72 md:right-40 lg:right-64 xl:right-96 lg:text-large xl:text-xl m-auto 
-            rounded-md lg:pt-4 xl:pt-8 pr-8 pl-8 xl:w-1/5 2xl:w-1/6 h-1/4 bg-white 2xl:leading-10 border-8 border-white"
+            className="box absolute bottom-72 md:right-40 lg:right-64 xl:right-96 lg:text-lg xl:text-xl m-auto 
+            rounded-md md:pt-6 lg:pt-7 xl:pt-8 pr-8 pl-8 lg:w-1/4 xl:w-1/5 2xl:w-1/6 h-1/4 bg-white leading-9 
+            border-8 border-white"
             initial={{ x: 300, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             exit={{ x: -300, opacity: 0 }}
