6a99cc9f89732b04fe910c536aabd7a7017cfe24
Andrew Shen <azshen23@gmail.com>
2023-08-09 23:07:06 -0400

    card finished

--- src/components/About/CardBack.jsx
+++ src/components/About/CardBack.jsx
@@ -1,4 +1,31 @@
 import { motion } from "framer-motion";
+import cppLogo from "/src/assets/images/About/logos/cppLogo.svg";
+import pythonLogo from "/src/assets/images/About/logos/pythonLogo.svg";
+import javascriptLogo from "/src/assets/images/About/logos/javascriptLogo.svg";
+import reactLogo from "/src/assets/images/About/logos/reactLogo.svg";
+import javaLogo from "/src/assets/images/About/logos/javaLogo.svg";
+import postgresLogo from "/src/assets/images/About/logos/postgresLogo.svg";
+import kotlinLogo from "/src/assets/images/About/logos/kotlinLogo.svg";
+import androidLogo from "/src/assets/images/About/logos/androidLogo.svg";
+import reactnativeLogo from "/src/assets/images/About/logos/reactnativeLogo.svg";
+import nextjsLogo from "/src/assets/images/About/logos/nextjsLogo.svg";
+import svelteLogo from "/src/assets/images/About/logos/svelteLogo.svg";
+import vueLogo from "/src/assets/images/About/logos/vueLogo.svg";
+
+const logos = [
+  pythonLogo,
+  cppLogo,
+  nextjsLogo,
+  javascriptLogo,
+  reactLogo,
+  svelteLogo,
+  postgresLogo,
+  kotlinLogo,
+  androidLogo,
+  reactnativeLogo,
+  javaLogo,
+  vueLogo,
+];
 
 function CardBack({ isFlipped }) {
   return (
@@ -6,10 +33,19 @@ function CardBack({ isFlipped }) {
       initial={{ rotateY: 180 }}
       animate={{ rotateY: isFlipped ? 180 : 0 }}
       transition={{ duration: 0.3 }}
-      className="back w-full h-full items-center flex flex-col"
+      className="back w-full h-full items-center flex flex-col pt-8 pl-10 pr-10"
     >
-      <div className="font-bold">
-        <h1>TECH STACK</h1>
+      <div className="flex flex-col font-bold w-full">
+        <h1 className="flex flex-col w-full text-center text-2xl">
+          TECH STACK
+        </h1>
+        <div class="grid grid-cols-4 gap-12 w-full  ">
+          {logos.map((link, index) => (
+            <div class="w-24" key={index}>
+              <img className="w-full h-full" src={link} />
+            </div>
+          ))}
+        </div>
       </div>
     </motion.div>
   );
