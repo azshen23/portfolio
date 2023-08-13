76361982be3e64a9b10cc6a87ed337b4242ad293
Andrew Shen <azshen23@gmail.com>
2023-08-13 15:19:25 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,9 @@ function Header() {
   return (
     <div className="box bg-white w-full h-20 flex flex-row justify-between items-center pl-20 pr-20 pokemonFont border-4 border-white">
       <div className="w-36">
-        <img src={logo} />
+        <Link to="/">
+          <img src={logo} />
+        </Link>
       </div>
       <div className="justify-between flex flex-row w-[45%]">
         <Link to="/" className="hover:underline text-xl font-bold">
