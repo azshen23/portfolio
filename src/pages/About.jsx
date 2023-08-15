733b98846796cdc1977d8d9c3d0f6ff42c7cfbb3
Andrew Shen <azshen23@gmail.com>
2023-08-15 19:03:12 -0400

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ function About() {
       <div className="flex flex-col w-full items-center">
         <motion.div
           onClick={toggleCard}
-          className="border-4 border-black rounded-xl w-4/6 lg:h-[600px] flex flex-col bg-box14 bg-cover pl-2 pt-1 pr-2 
+          className="border-4 border-black rounded-xl w-[90%] md:w-5/6 lg:w-4/6 lg:h-[600px] flex flex-col bg-box14 bg-cover pl-2 pt-1 pr-2 
           hover:cursor-pointer pokemonFont text-sm lg:text-lg xl:text-2xl"
           transition={{ duration: 0.3 }}
           whileHover={{ scale: 1.05 }}
